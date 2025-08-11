de2775d298f50f254d08e098fddc3da805cebcb4
HenriqueMayer <henriquermayer@gmail.com>
2025-08-11 15:29:16 -0300

    Add own tools script with temperature conversion and agent setup for weather forecasting

--- scripts/1.1.researcher.py
+++ scripts/1.1.researcher.py
@@ -1,15 +1,1 @@
-from agno.agent import Agent
-from agno.models.groq import Groq
-from agno.tools.tavily import TavilyTools
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-agent = Agent(
-    model=Groq(id="llama-3.1-8b-instant"), tools=[TavilyTools()], debug_mode=True
-)
-
-agent.print_response(
-    "Use your tools to search for the Porto Alegre/Brazil weather forecast for today."
-)
+c
